fe67a9a65fb4e512e315fddff39253c6a9ea4123
omallassi <olivier.mallassi@gmail.com>
2022-03-05 14:01:34 +0100

    #48

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -13,7 +13,7 @@ use prettytable::{Cell, Row, Table};
 extern crate lazy_static;
 
 extern crate clap;
-use clap::{App, AppSettings, Arg, SubCommand};
+use clap::{Arg, Command};
 
 extern crate dirs;
 
@@ -188,37 +188,37 @@ fn init() -> Result<()> {
 /// The main program - start the CLI ...
 fn main() {
     //
-    let _options = App::new("adr")
+    let cmd = Command::new("adr")
         .version("0.1.0")
         .about("A CLI to help you manage your ADR in git")
         .subcommand(
-            SubCommand::with_name("list")
+            Command::new("list")
                 .about("Lists all Decision Records")
                 .version("0.1.0"),
         )
         .subcommand(
-            SubCommand::with_name("init")
+            Command::new("init")
                 .about("Init ADRust based on config")
                 .version("0.1.0"),
         )
         .subcommand(
-            App::new("config")
+            Command::new("config")
                 .about("Manage Configuration Items")
-                .setting(AppSettings::SubcommandRequiredElseHelp)
+                .subcommand_required(true)
                 .subcommand(
-                    SubCommand::with_name("set")
+                    Command::new("set")
                         .about("Update Configuration Item with specified value")
                         .arg(
-                            Arg::with_name("name")
-                                .short("n")
+                            Arg::new("name")
+                                .short('n')
                                 .long("name")
                                 .required(true)
                                 .takes_value(true)
                                 .help("the name of the property"),
                         )
                         .arg(
-                            Arg::with_name("value")
-                                .short("v")
+                            Arg::new("value")
+                                .short('v')
                                 .long("value")
                                 .required(true)
                                 .takes_value(true)
@@ -226,34 +226,34 @@ fn main() {
                         ),
                 )
                 .subcommand(
-                    SubCommand::with_name("list").about("List All the Configuration Items"),
+                    Command::new("list").about("List All the Configuration Items"),
                 ),
         )
         .subcommand(
-            App::new("tags")
+            Command::new("tags")
                 .about("Manage Tags")
-                .setting(AppSettings::SubcommandRequiredElseHelp)
-                .subcommand(SubCommand::with_name("list").about("List All the Tags")),
+                .subcommand_required(true)
+                .subcommand(Command::new("list").about("List All the Tags")),
         )
         .subcommand(
-            App::new("lf")
+            Command::new("lf")
                 .about("Manages ADRs lifecycle")
-                .setting(AppSettings::SubcommandRequiredElseHelp)
+                .subcommand_required(true)
                 .subcommand(
-                    SubCommand::with_name("new")
+                    Command::new("new")
                         .about("Creates a new Decision Record")
                         .version("0.1.0")
                         .arg(
-                            Arg::with_name("title")
-                                .short("t")
+                            Arg::new("title")
+                                .short('t')
                                 .long("title")
                                 .takes_value(true)
                                 .required(true)
                                 .help("Give the title of your Decision Record"),
                         )
                         .arg(
-                            Arg::with_name("path")
-                                .short("p")
+                            Arg::new("path")
+                                .short('p')
                                 .long("path")
                                 .takes_value(true)
                                 .required(false)
@@ -261,12 +261,12 @@ fn main() {
                         ),
                 )
                 .subcommand(
-                    SubCommand::with_name("decided")
+                    Command::new("decided")
                         .about("update the Status to Decide")
                         .version("0.1.0")
                         .arg(
-                            Arg::with_name("path")
-                                .short("p")
+                            Arg::new("path")
+                                .short('p')
                                 .long("path")
                                 .takes_value(true)
                                 .required(true)
@@ -274,20 +274,20 @@ fn main() {
                         ),
                 )
                 .subcommand(
-                    SubCommand::with_name("superseded-by")
+                    Command::new("superseded-by")
                         .about("Supersede the decision with another decision")
                         .version("0.1.0")
                         .arg(
-                            Arg::with_name("path")
-                                .short("p")
+                            Arg::new("path")
+                                .short('p')
                                 .long("path")
                                 .takes_value(true)
                                 .required(true)
                                 .help("Give the path of your Decision Record"),
                         )
                         .arg(
-                            Arg::with_name("by")
-                                .short("b")
+                            Arg::new("by")
+                                .short('b')
                                 .long("by")
                                 .takes_value(true)
                                 .required(true)
@@ -295,20 +295,20 @@ fn main() {
                         ),
                 )
                 .subcommand(
-                    SubCommand::with_name("completed-by")
+                    Command::new("completed-by")
                         .about("Complete a decision with another decision")
                         .version("0.1.0")
                         .arg(
-                            Arg::with_name("path")
-                                .short("p")
+                            Arg::new("path")
+                                .short('p')
                                 .long("path")
                                 .takes_value(true)
                                 .required(true)
                                 .help("Give the path of the DR which is completed by"),
                         )
                         .arg(
-                            Arg::with_name("by")
-                                .short("b")
+                            Arg::new("by")
+                                .short('b')
                                 .long("by")
                                 .takes_value(true)
                                 .required(true)
@@ -316,12 +316,12 @@ fn main() {
                         ),
                 )
                 .subcommand(
-                    SubCommand::with_name("obsoleted")
+                    Command::new("obsoleted")
                         .about("Uodate the Status to Obsoleted")
                         .version("0.1.0")
                         .arg(
-                            Arg::with_name("path")
-                                .short("p")
+                            Arg::new("path")
+                                .short('p')
                                 .long("path")
                                 .takes_value(true)
                                 .required(true)
@@ -330,38 +330,41 @@ fn main() {
                 ),
         )
         .subcommand(
-            SubCommand::with_name("search")
+            Command::new("search")
                 .about("Search across all ADRs")
                 .version("0.1.0")
                 .args(&[
-                    Arg::with_name("query")
-                        .short("q")
+                    Arg::new("query")
+                        .short('q')
                         .long("query")
                         .takes_value(true)
                         .required(true)
                         .conflicts_with("build-index")
                         .help("Provide your search query"),
-                    Arg::with_name("build-index")
-                        .short("b")
+                    Arg::new("build-index")
+                        .short('b')
                         .long("build-index")
                         .takes_value(false)
                         .required(true)
                         .conflicts_with("query")
                         .help("Build the index based on available ADRs."),
                 ]),
-        )
-        .get_matches();
+        );
 
     //
-    match _options.subcommand() {
-        ("list", Some(_matches)) => {
+    let _matches = cmd.get_matches();
+    let subcommand = _matches.subcommand();
+
+
+    match subcommand {
+        Some(("list", _matches)) => {
             list_all_adr().unwrap();
         }
-        ("init", Some(_matches)) => {
+        Some(("init", _matches)) => {
             init().unwrap();
         }
-        ("lf", Some(matches)) => match matches.subcommand() {
-            ("new", Some(matches)) => {
+        Some(("lf", matches)) => match matches.subcommand() {
+            Some(("new", matches)) => {
                 if matches.is_present("title") {
                     adr_core::adr_repo::create_adr(
                         adr_config::config::get_config(),
@@ -371,7 +374,7 @@ fn main() {
                     .unwrap();
                 }
             }
-            ("decided", Some(set_matches)) => {
+            Some(("decided", set_matches)) => {
                 if set_matches.is_present("path") {
                     let file_path = set_matches.value_of("path").unwrap();
                     let cfg: AdrToolConfig = adr_config::config::get_config();
@@ -380,7 +383,7 @@ fn main() {
                     adr_core::adr_repo::transition_to_decided(base_path, file_path).unwrap();
                 }
             }
-            ("completed-by", Some(set_matches)) => {
+            Some(("completed-by", set_matches)) => {
                 if set_matches.is_present("path") && set_matches.is_present("by") {
                     let cfg: AdrToolConfig = adr_config::config::get_config();
                     let base_path = Path::new(&cfg.adr_src_dir);
@@ -391,7 +394,7 @@ fn main() {
                         .unwrap();
                 }
             }
-            ("superseded-by", Some(set_matches)) => {
+            Some(("superseded-by", set_matches)) => {
                 if set_matches.is_present("path") && set_matches.is_present("by") {
                     let cfg: AdrToolConfig = adr_config::config::get_config();
                     let base_path = Path::new(&cfg.adr_src_dir);
@@ -402,7 +405,7 @@ fn main() {
                         .unwrap();
                 }
             }
-            ("obsoleted", Some(set_matches)) => {
+            Some(("obsoleted", set_matches)) => {
                 if set_matches.is_present("path") {
                     let cfg: AdrToolConfig = adr_config::config::get_config();
                     let base_path = Path::new(&cfg.adr_src_dir);
@@ -414,11 +417,11 @@ fn main() {
 
             _ => unreachable!(),
         },
-        ("config", Some(config_matches)) => match config_matches.subcommand() {
-            ("list", Some(_remote_matches)) => {
+        Some(("config", config_matches)) => match config_matches.subcommand() {
+            Some(("list", _remote_matches)) => {
                 list_all_config().unwrap();
             }
-            ("set", Some(set_matches)) => {
+            Some(("set", set_matches)) => {
                 set_config(
                     set_matches.value_of("name").unwrap(),
                     set_matches.value_of("value").unwrap(),
@@ -427,13 +430,13 @@ fn main() {
             }
             _ => unreachable!(),
         },
-        ("tags", Some(tags_matches)) => match tags_matches.subcommand() {
-            ("list", Some(_remote_matches)) => {
+        Some(("tags", tags_matches)) => match tags_matches.subcommand() {
+            Some(("list", _remote_matches)) => {
                 list_all_tags().unwrap();
             }
             _ => unreachable!(),
         },
-        ("search", Some(search_matches)) => {
+        Some(("search", search_matches)) => {
             if search_matches.is_present("query") {
                 let query = search_matches.value_of("query").unwrap().to_string();
                 search(query).unwrap();
@@ -443,7 +446,7 @@ fn main() {
             }
         }
 
-        ("", None) => println!("No subcommand was used"), // If no subcommand was usd it'll match the tuple ("", None)
+        //Some("", None) => println!("No subcommand was used"), // If no subcommand was usd it'll match the tuple ("", None)
         _ => unreachable!(), // If all subcommands are defined above, anything else is unreachabe!()
     }
 }
