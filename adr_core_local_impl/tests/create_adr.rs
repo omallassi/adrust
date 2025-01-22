c70d3b55c62097c3a25de44f5c0f9481997d739e
omallassi <olivier.mallassi@gmail.com>
2025-01-22 11:06:05 +0100

    update cucumber(-rs) to latest version

--- adr_core_local_impl/tests/create_adr.rs
+++ adr_core_local_impl/tests/create_adr.rs
@@ -1,17 +1,22 @@
-use cucumber::{after, before, cucumber};
+use cucumber::{given, then, when, World};
 
 extern crate directories;
 use directories::ProjectDirs;
 
 use std::fs;
+use std::future::Future;
+use std::path::{Path, PathBuf};
+use chrono::Utc;
+use walkdir::WalkDir;
+use adr_core::adr_repo::{Status, TransitionStatus};
 
+#[derive(Debug, World)]
 pub struct AdrNames {
     name: String,
     has_transitioned: bool,
     base_path: String,
+    scenario_name: String
 }
-
-impl cucumber::World for AdrNames {}
 
 impl std::default::Default for AdrNames {
     fn default() -> AdrNames {
@@ -19,6 +24,7 @@ impl std::default::Default for AdrNames {
             name: "default-name".to_string(),
             has_transitioned: false,
             base_path: String::from(""),
+            scenario_name: "default-name".to_string(),
         }
     }
 }
@@ -28,8 +34,8 @@ mod helper {
     extern crate directories;
     use directories::ProjectDirs;
 
-    pub fn create_decision(name: &str) -> io::Result<bool> {
-        let project_dirs: ProjectDirs = match ProjectDirs::from("murex", "adrust-tool", "test") {
+    pub fn create_decision(name: &str, scenario_name: &String) -> io::Result<bool> {
+        let project_dirs: ProjectDirs = match ProjectDirs::from("murex", "adrust-tool", self::get_workspace(scenario_name).as_str()) {
             None => panic!("issue while preparing test"),
             Some(project_dirs) => project_dirs,
         };
@@ -56,241 +62,187 @@ mod helper {
 
         Ok(adr_core::adr_repo::create_adr(cfg, None, name).unwrap())
     }
+
+    pub fn get_workspace(scenario_name: &String) -> String {
+        let val = format!("test-{}", scenario_name.replace(" ","-"));
+
+        return val.clone();
+    }
 }
 
-mod create_new_adr_steps {
-    use cucumber::steps;
-    use std::fs;
-    extern crate directories;
-    use directories::ProjectDirs;
 
-    extern crate adr_core;
-    use crate::helper;
-
-    steps! (crate::AdrNames => {
-        given regex r"^A decision (.+) I need to make$" (String) |adr, name, _step| {
-            adr.name = name.to_string();
-        };
-
-        when "I create a new Decision Record" |_adr, _step| {
-
-        };
-
-        then regex r"A new file named (.+) is created$" (String) |adr, name, _step| {
-            let is_created = helper::create_decision(&adr.name).unwrap();
-            assert_eq!(is_created, true);
-
-            // TODO there is certainly a way to return project_dir as part of create_decision
-            let project_dirs: ProjectDirs = match ProjectDirs::from("murex", "adrust-tool", "test") {
-                None => panic!("issue while preparing test"),
-                Some(project_dirs) => project_dirs
-            };
-            let t = project_dirs.cache_dir().join(
-                format!("src/{}.adoc", name.to_string())
-            );
-            //
-            let expected_path = t.as_path();
-            assert_eq!(expected_path.exists(), true);
-            //
-            let content: String = fs::read_to_string(expected_path).unwrap();
-            assert!(content.contains("{wip}"));
-        };
-
-    });
+#[given(regex=r"^A decision (.+) I need to make$")]
+fn for_this_decision(world: &mut AdrNames, decision_name: String) {
+    world.name = decision_name;
 }
 
-mod create_already_adr_steps {
-    use cucumber::steps;
-    extern crate adr_core;
-    use crate::helper;
-
-    steps! (crate::AdrNames => {
-        given regex r"^A new decision (.+) that already exists$" (String) |adr, name, _step| {
-            adr.name = name.to_string();
-            //the decision should already exist so we create it.
-            helper::create_decision(&adr.name).unwrap();
-        };
-
-        when "I create a new ADR" |_adr, _step| {
-
-        };
-
-        then "The creation fails" | adr, _step | {
-            //create the same file
-            let is_created = helper::create_decision(&adr.name).unwrap();
-            assert_eq!(is_created, false);
-        };
-    });
+#[when("I create a new Decision Record")]
+fn list_all_tags(world: &mut AdrNames) {
+    //do nothing
 }
 
-mod check_transitions_and_lifecycle_of_adr {
-    use cucumber::steps;
-    use std::fs;
-    use std::path::Path;
 
-    extern crate directories;
-    use directories::ProjectDirs;
+#[then(regex=r"A new file named (.+) is created$")]
+fn check_tags(adr: &mut AdrNames, name: String) {
+    let is_created = helper::create_decision(&adr.name, &adr.scenario_name).unwrap();
+    assert_eq!(is_created, true);
 
-    extern crate adr_core;
-    use adr_core::adr_repo::*;
-
-    use walkdir::WalkDir;
-
-    use chrono::prelude::*;
-    use std::path::PathBuf;
-
-    steps! (crate::AdrNames => {
-
-        given regex r"^a decision with status (.+)$" (String) |adr, status, _step| {
-
-            let project_dirs: ProjectDirs = match ProjectDirs::from("murex", "adrust-tool", "test") {
-                None => panic!("issue while preparing test"),
-                Some(project_dirs) => project_dirs
-            };
-
-            //copy all files to ease the different transitions
-            let rep = "./tests/data/";
-            let srcdir = PathBuf::from(rep);
-
-            for entry in WalkDir::new(srcdir.as_path()).into_iter().filter_map(|e| e.ok() ) {
-                if ! entry.path().is_dir() {
-                    let from = entry.path();
-                    let file_name = entry.path().file_name().unwrap();
-                    let to = project_dirs.cache_dir().join("src").join(file_name);
-
-                    println!("[init] Want to copy file [{:?}] to [{:?}]", from, to);
-                    match fs::copy(from, to.as_path()) {
-                        Ok(_) => (),
-                        Err(why) => panic!("{:?}", why),
-                    };
-                }
-            }
-
-            adr.base_path = format!("{}", project_dirs.cache_dir().join("src").display());
-            adr.name = format!("{}", project_dirs.cache_dir().join("src").join(status).with_extension("adoc").display());
-            println!("The current scenario will use adr path [{}]", adr.name);
-        };
-
-        when regex r"^the decision is transitioned to (.+) by (.+)$" (String, String) |adr, transition, by, _step| {
-            match "n/a" == by {
-                true => {
-                    println!("calling transition_to() with [{}] [{}] [{}]", transition, adr.name, by);
-                    match adr_core::adr_repo::transition_to(TransitionStatus::from_str(transition), Path::new(&adr.base_path), adr.name.as_str(), "") {
-                        Ok(transitioned) => adr.has_transitioned = transitioned,
-                        Err(why) => panic!("{:?}", why)
-                    };
-                },
-                false => {
-                    let by = format!("{}", PathBuf::from(adr.base_path.as_str()).join(by).display());
-                    println!("calling transition_to() with [{}] [{}] [{}]", transition, adr.name, by);
-                    match adr_core::adr_repo::transition_to(TransitionStatus::from_str(transition), Path::new(&adr.base_path), adr.name.as_str(), by.as_str()) {
-                        Ok(transitioned) => adr.has_transitioned = transitioned,
-                        Err(why) => panic!("{:?}", why)
-                    };
-                },
-            };
-        };
-
-        then regex r"^the transition is (.+)$" (bool) |adr, has_transition, _step| {
-            //compare expected with actual
-            assert_eq!(has_transition, adr.has_transitioned)
-        };
-
-        then regex r"^the new status is (.+) by (.+)$" (String, String) |adr, new_status, by, _step| {
-            let new_adr = match adr_core::adr_repo::build_adr(Path::new(&adr.base_path), Path::new(adr.name.as_str())) {
-                Ok(adr) => adr,
-                Err(why) => panic!("{:?}", why),
-            };
-
-            assert_eq!(Status::from_str(new_status), new_adr.status);
-            if "n/a" != by && adr.has_transitioned {
-                let by = format!("}} {}", by);
-                assert_eq!(true, new_adr.content.contains(&by));
-            }
-        };
-
-        then regex r"^the date is updated to today if (.+) is true$" (bool) |adr, is_accepted, _step| {
-            let adr = match adr_core::adr_repo::build_adr(Path::new(&adr.base_path), Path::new(adr.name.as_str())) {
-                Ok(adr) => adr,
-                Err(why) => panic!("{:?}", why),
-            };
-
-            if is_accepted {
-                assert_eq!(adr.date, Utc::now().date_naive().format("%Y-%m-%d").to_string());
-            }
-            else {
-                assert_eq!(adr.date, "2019-10-28");
-            }
-        };
-
-    });
-}
-
-mod check_tags_management {
-    use cucumber::steps;
-    extern crate adr_core;
-    use std::path::Path;
-
-    steps! (crate::AdrNames => {
-        given regex r"^the decision (.+)$" (String) |adr, decision_name, _step| {
-            adr.name = decision_name;
-        };
-
-        when "I list all the tags" |_adr, _step| {
-            //nothing to do really
-        };
-
-        then regex r"^I got (.+), (.+), (.+) tags$" (String, String, String) |adr, tag_1, tag_2, tag_3, _step| {
-            let file_path = Path::new(adr.name.as_str());
-            let decision = adr_core::adr_repo::build_adr(Path::new(""), file_path).unwrap();
-
-            if decision.tags_array.len() == 1 {
-                assert_eq!(decision.tags_array[0].trim(), tag_1);
-            }
-            if decision.tags_array.len() == 2 {
-                assert_eq!(decision.tags_array[1].trim(), tag_2);
-            }
-            if decision.tags_array.len() == 3 {
-                assert_eq!(decision.tags_array[2].trim(), tag_3);
-            }
-        };
-    });
-}
-
-before!(a_before_fn => |_scenario| {
-    let project_dirs: ProjectDirs = match ProjectDirs::from("murex", "adrust-tool", "test") {
+    // TODO there is certainly a way to return project_dir as part of create_decision
+    let project_dirs: ProjectDirs = match ProjectDirs::from("murex", "adrust-tool", helper::get_workspace(&adr.scenario_name).as_str()) {
         None => panic!("issue while preparing test"),
         Some(project_dirs) => project_dirs
     };
-    fs::create_dir_all(project_dirs.cache_dir().join("src").as_path()).unwrap();
-    fs::create_dir_all(project_dirs.cache_dir().join("templates").as_path()).unwrap();
+    let t = project_dirs.cache_dir().join(
+        format!("src/{}.adoc", name.to_string())
+    );
+    //
+    let expected_path = t.as_path();
+    assert_eq!(expected_path.exists(), true);
+    //
+    let content: String = fs::read_to_string(expected_path).unwrap();
+    assert!(content.contains("{wip}"));
+}
 
-    fs::copy("./tests/adr-template-v0.1.adoc", project_dirs.cache_dir().join("templates/adr-template-v0.1.adoc").as_path()).unwrap();
-});
 
-after!(an_after_fn => |_scenario| {
-    let project_dirs: ProjectDirs = match ProjectDirs::from("murex", "adrust-tool", "test") {
+#[given(regex=r"^A new decision (.+) that already exists$")]
+fn new_decision_exists(adr: &mut AdrNames, name: String) {
+    adr.name = name.to_string();
+    //the decision should already exist so we create it.
+    helper::create_decision(&adr.name, &adr.scenario_name).unwrap();
+}
+
+#[when("I create a new ADR")]
+fn create_new_adr(adr: &mut AdrNames) {
+    //do nothing
+}
+
+#[then("The creation fails")]
+fn new_adr_creation_fails(adr: &mut AdrNames) {
+    //create the same file
+    let is_created = helper::create_decision(&adr.name, &adr.scenario_name).unwrap();
+    assert_eq!(is_created, false);
+}
+
+
+
+#[given(regex=r"^a decision with status (.+)$")]
+fn a_decision_with_status(adr: &mut AdrNames, status: String) {
+
+    let project_dirs: ProjectDirs = match ProjectDirs::from("murex", "adrust-tool", helper::get_workspace(&adr.scenario_name).as_str()) {
         None => panic!("issue while preparing test"),
         Some(project_dirs) => project_dirs
     };
-    fs::remove_dir_all(project_dirs.cache_dir()).unwrap();
-});
 
-fn setup() {}
+    //copy all files to ease the different transitions
+    let rep = "./tests/data/";
+    let srcdir = PathBuf::from(rep);
 
-cucumber! {
-    features: "./features/adr_lifecycle",
-    world: crate::Adr,
-    steps: &[
-        create_new_adr_steps::steps,
-        create_already_adr_steps::steps,
-        check_transitions_and_lifecycle_of_adr::steps,
-        check_tags_management::steps,
-        // update_adr_decided_steps::steps,
-        // not_update_adr_decided_steps::steps,
-        // transition_adr_to_completed_steps::steps,
-    ],
-    setup: setup,
-    before: &[a_before_fn],
-    after: &[an_after_fn]
+    for entry in WalkDir::new(srcdir.as_path()).into_iter().filter_map(|e| e.ok() ) {
+        if ! entry.path().is_dir() {
+            let from = entry.path();
+            let file_name = entry.path().file_name().unwrap();
+            let to = project_dirs.cache_dir().join("src").join(file_name);
+
+            println!("[init] Want to copy file [{:?}] to [{:?}]", from, to);
+            match fs::copy(from, to.as_path()) {
+                Ok(_) => (),
+                Err(why) => panic!("{:?}", why),
+            };
+        }
+    }
+
+    adr.base_path = format!("{}", project_dirs.cache_dir().join("src").display());
+    adr.name = format!("{}", project_dirs.cache_dir().join("src").join(status).with_extension("adoc").display());
+    println!("The current scenario will use adr path [{}]", adr.name);
+}
+
+#[when(regex=r"^the decision is transitioned to (.+) by (.+)$")]
+fn decision_is_transitioned(adr: &mut AdrNames, transition: String, by: String) {
+    match "n/a" == by {
+        true => {
+            println!("calling transition_to() with [{}] [{}] [{}]", transition, adr.name, by);
+            match adr_core::adr_repo::transition_to(TransitionStatus::from_str(transition), Path::new(&adr.base_path), adr.name.as_str(), "") {
+                Ok(transitioned) => adr.has_transitioned = transitioned,
+                Err(why) => panic!("{:?}", why)
+            };
+        },
+        false => {
+            let by = format!("{}", PathBuf::from(adr.base_path.as_str()).join(by).display());
+            println!("calling transition_to() with [{}] [{}] [{}]", transition, adr.name, by);
+            match adr_core::adr_repo::transition_to(TransitionStatus::from_str(transition), Path::new(&adr.base_path), adr.name.as_str(), by.as_str()) {
+                Ok(transitioned) => adr.has_transitioned = transitioned,
+                Err(why) => panic!("{:?}", why)
+            };
+        },
+    };
+}
+
+#[then(regex=r"^the transition is (.+)$")]
+fn the_transition_is(adr: &mut AdrNames, has_transition: bool) {
+    //compare expected with actual
+    assert_eq!(has_transition, adr.has_transitioned)
+}
+
+#[then(regex=r"^the new status is (.+) by (.+)$")]
+fn the_new_status_is(adr: &mut AdrNames, new_status: String, by: String) {
+    let new_adr = match adr_core::adr_repo::build_adr(Path::new(&adr.base_path), Path::new(adr.name.as_str())) {
+        Ok(adr) => adr,
+        Err(why) => panic!("{} - {} - {:?}", &adr.base_path, &adr.name.as_str(), why),
+    };
+
+    assert_eq!(Status::from_str(new_status), new_adr.status);
+    if "n/a" != by && adr.has_transitioned {
+        let by = format!("}} {}", by);
+        assert_eq!(true, new_adr.content.contains(&by));
+    }
+}
+
+#[then(regex=r"^the date is updated to today if (.+) is true$")]
+fn the_date_is_updated_to_today_if_is_true(adr: &mut AdrNames, is_accepted: bool) {
+    let adr = match adr_core::adr_repo::build_adr(Path::new(&adr.base_path), Path::new(adr.name.as_str())) {
+        Ok(adr) => adr,
+        Err(why) => panic!("{:?}", why),
+    };
+
+    if is_accepted {
+        assert_eq!(adr.date, Utc::now().date_naive().format("%Y-%m-%d").to_string());
+    }
+    else {
+        assert_eq!(adr.date, "2019-10-28");
+    }
+}
+
+fn main() {
+    futures::executor::block_on(
+        AdrNames::cucumber()
+            .before(|_feature, _rule, scenario, world| {
+                Box::pin(async move {
+                    world.scenario_name = format!("{}-{}", scenario.name.clone(), scenario.position.line.clone());
+
+                    let project_dirs: ProjectDirs = match ProjectDirs::from("murex", "adrust-tool", helper::get_workspace(&world.scenario_name).as_str()) {
+                        None => panic!("issue while preparing test"),
+                        Some(project_dirs) => project_dirs
+                    };
+                    fs::create_dir_all(project_dirs.cache_dir().join("src").as_path()).unwrap();
+                    fs::create_dir_all(project_dirs.cache_dir().join("templates").as_path()).unwrap();
+
+                    fs::copy("./tests/adr-template-v0.1.adoc", project_dirs.cache_dir().join("templates/adr-template-v0.1.adoc").as_path()).unwrap();
+                })
+            })
+            .after(|_feature, _rule, scenario, _ev, world| {
+                Box::pin(async move {
+                    let project_dirs: ProjectDirs = match ProjectDirs::from("murex", "adrust-tool", helper::get_workspace(&scenario.name).as_str()) {
+                        None => panic!("issue while preparing test"),
+                        Some(project_dirs) => project_dirs
+                    };
+                    if project_dirs.cache_dir().exists() {
+                        fs::remove_dir_all(project_dirs.cache_dir()).unwrap();
+                    }
+                    else{
+                        println!("{} doest not exist", project_dirs.cache_dir().display());
+                    }
+                })
+            })
+            .run_and_exit("features/adr_lifecycle/adr_lifecycle_management.feature")
+    );
 }
---
+++ adr_core_local_impl/tests/tags_with_adr.rs
@@ -0,0 +1,65 @@
+use cucumber::{World};
+
+extern crate directories;
+use directories::ProjectDirs;
+
+use std::fs;
+use std::future::Future;
+
+#[derive(Debug, World)]
+pub struct AdrNames {
+    name: String,
+    has_transitioned: bool,
+    base_path: String,
+}
+
+impl std::default::Default for AdrNames {
+    fn default() -> AdrNames {
+        AdrNames {
+            name: "default-name".to_string(),
+            has_transitioned: false,
+            base_path: String::from(""),
+        }
+    }
+}
+mod check_tags_management {
+    use cucumber::{given, then, when, World};
+    extern crate adr_core;
+    use std::path::Path;
+    use crate::AdrNames;
+
+    #[given(regex = r"^the decision (.+)$")]
+    fn for_this_decision(world: &mut AdrNames, decision_name: String) {
+        world.name = decision_name;
+    }
+
+    #[when("I list all the tags")]
+    fn list_all_tags(world: &mut AdrNames) {
+        //nothign to do
+    }
+
+    #[then(regex = r"^I got (.+), (.+), (.+) tags$")]
+    fn check_tags(adr: &mut AdrNames, tag_1: String, tag_2: String, tag_3: String) {
+        let file_path = Path::new(adr.name.as_str());
+        let decision = adr_core::adr_repo::build_adr(Path::new(""), file_path).unwrap();
+
+        if decision.tags_array.len() == 1 {
+            assert_eq!(decision.tags_array[0].trim(), tag_1);
+        }
+        if decision.tags_array.len() == 2 {
+            assert_eq!(decision.tags_array[1].trim(), tag_2);
+        }
+        if decision.tags_array.len() == 3 {
+            assert_eq!(decision.tags_array[2].trim(), tag_3);
+        }
+    }
+}
+
+fn main() {
+    // You may choose any executor you like (`tokio`, `async-std`, etc.).
+    // You may even have an `async` main, it doesn't matter. The point is that
+    // Cucumber is composable. :)
+    futures::executor::block_on(AdrNames::run(
+        "features/adr_lifecycle/adr_tags_management.feature",
+    ));
+}
